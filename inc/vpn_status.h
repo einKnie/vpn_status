72b329df34828865943863c76edf266ddfefa4dc
einKnie <einKnie@gmx.at>
2021-01-21 21:15:48 +0100

    added some comments

--- inc/vpn_status.h
+++ inc/vpn_status.h
@@ -8,6 +8,7 @@
 #define VPN_DOWN 0
 #define VPN_SYMBOL ""
 
+/// interfaces linked list
 typedef struct ifdata {
 	char ifname[IFNAMSIZ];
 	char mac[64];
@@ -15,14 +16,27 @@ typedef struct ifdata {
 	struct ifdata *prev;
 } ifdata_t;
 
+
+/// Initialize vpn_status
 int init(void);
+/// Parse a netlink message
 int parse_nlmsg(char *nlmsg_buf, ssize_t buflen, ifdata_t *p_head);
+/// request info on all interfaces && store data
 int fetch_ifinfo(ifdata_t **head);
-void print_ifinfo(ifdata_t *head);
-void add_ifdata(ifdata_t *p_new, ifdata_t **head);
-void del_ifdata(ifdata_t *p_del, ifdata_t **head);
+/// fetch single interface data from a netlink message
 ifdata_t *get_ifdata(struct nlmsghdr *hdr);
+
+/// print info on interfaces
+void print_ifinfo(ifdata_t *head);
+/// add an entry to the interfaces linked list
+void add_ifdata(ifdata_t *p_new, ifdata_t **head);
+/// remove an entry from the interfaces linked list
+void del_ifdata(ifdata_t *p_del, ifdata_t **head);
+/// find full data of query in interfaces linked list
 ifdata_t *find_ifdata(ifdata_t *ifquery, ifdata_t *head);
+
+/// write the symbol to the file
+/// or remove it
 int file_write(int op);
 
 #endif // !_VPN_STATUS_H_
